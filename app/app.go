bc508bb0186bb1fb9d35e8781a9d84647b738d99
Julien Plée <julplee@gmail.com>
2019-08-07 19:34:42 +0200

    Create app.go

---
+++ app/app.go
@@ -0,0 +1,56 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"../config"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+)
+
+// App has a Router and a Database
+type App struct {
+	Router *mux.Router
+	DB     *gorm.DB
+}
+
+// Initialize initializes the app with predefined configuration
+func (a *App) Initialize(config *config.Config) {
+	dbURI := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=True",
+		config.DB.Username,
+		config.DB.Password,
+		config.DB.Host,
+		config.DB.Port,
+		config.DB.Name,
+		config.DB.Charset)
+
+	db, err := gorm.Open(config.DB.Dialect, dbURI)
+	if err != nil {
+		log.Fatal("Could not connect to database")
+	}
+
+	// a.DB =
+	a.Router = mux.NewRouter()
+	a.setRouters()
+}
+
+func (a *App) setRouters() {
+	a.Get("/pm25", a.handleRequest(handler.GetAllPM25))
+}
+
+// Get wraps the router for GET HTTP method
+func (a *App) Get(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	a.Router.HandleFunc(path, f).Methods("GET")
+}
+
+// RequestHandlerFunction defines a handler function for an HTTP request
+type RequestHandlerFunction func(db *gorm.DB, w http.ResponseWriter, r *http.Request)
+
+func (a *App) handleRequest(handler RequestHandlerFunction) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		handler(a.DB, w, r)
+	}
+}
