ff1cd7a41e3dd4807ee63bd6229c4e60ec23b790
Julien Plée <julplee@gmail.com>
2020-12-09 15:13:18 +0100

    respect json convention

--- app/model/model.go
+++ app/model/model.go
@@ -7,12 +7,12 @@ import (
 
 type PM25 struct {
 	gorm.Model
-	Value float64 `json:value`
+	Value float64 `json:"value"`
 }
 
 type PM10 struct {
 	gorm.Model
-	Value float64 `json:value`
+	Value float64 `json:"value"`
 }
 
 func DBMigrate(db *gorm.DB) *gorm.DB {
